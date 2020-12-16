261fef3b29a875693f2aa23974500a2e148b546c
eric <eric@HYPERION>
2020-12-16 14:57:46 +0000

    JPA Many to Many

--- src/main/java/ci/orbit/schoolproject/entities/Matiere.java
+++ src/main/java/ci/orbit/schoolproject/entities/Matiere.java
@@ -1,16 +1,19 @@
 package ci.orbit.schoolproject.entities;
 
 import java.io.Serializable;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Matiere implements Serializable {
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String designation;
-	
+	@ManyToMany(mappedBy = "matieres")
+	Set<Professeur> professeurs;
 }
--- src/main/java/ci/orbit/schoolproject/entities/Professeur.java
+++ src/main/java/ci/orbit/schoolproject/entities/Professeur.java
@@ -1,11 +1,21 @@
 package ci.orbit.schoolproject.entities;
 
 import java.io.Serializable;
+import java.util.Set;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Professeur extends Personne implements Serializable{
 	private String matricule;
 	private Long contact;
+	@ManyToMany
+	@JoinTable(
+		name = "prof_matiere",
+		joinColumns = @JoinColumn(name="professeur_id"),
+		inverseJoinColumns = @JoinColumn(name="matiere_id"))
+	Set<Matiere> matieres;
 }
